6f40df6a28aa918418907c00b2d2611a55c5c7cb
max <max.smith@duke.edu>
2020-07-29 22:30:55 -0400

    Created User profile

---
+++ src/main/java/com/ece350/assembler/profile/UserProfile.java
@@ -0,0 +1,58 @@
+package com.ece350.assembler.profile;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class UserProfile {
+
+    private UUID userProfileId;
+    private String username;
+    private String userProfileImageLink; // S3 key
+
+    public UserProfile(UUID userProfileId,
+                       String username,
+                       String userProfileImageLink) {
+        this.userProfileId = userProfileId;
+        this.username = username;
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    public UUID getUserProfileId() {
+        return userProfileId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUserProfileId(UUID userProfileId) {
+        this.userProfileId = userProfileId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUserProfileImageLink(String userProfileImageLink) {
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    public String getUserProfileImageLink() {
+        return userProfileImageLink;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserProfile that = (UserProfile) o;
+        return Objects.equals(userProfileId, that.userProfileId) &&
+                Objects.equals(username, that.username) &&
+                Objects.equals(userProfileImageLink, that.userProfileImageLink);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userProfileId, username, userProfileImageLink);
+    }
+}
