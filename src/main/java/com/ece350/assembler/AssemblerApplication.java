1f2fea2ec9f758c4d0f5dec10815fc79a7751698
max <max.smith@duke.edu>
2020-07-29 21:36:22 -0400

    first commit

---
+++ src/main/java/com/ece350/assembler/AssemblerApplication.java
@@ -0,0 +1,13 @@
+package com.ece350.assembler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ece350/assembler/AssemblerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ece350.assembler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
