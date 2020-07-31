0d0e9b1dc63bff89efb85532b4dac40f65c33b00
max <max.smith@duke.edu>
2020-07-30 23:15:01 -0400

    Created bootstrap react front end

--- src/main/java/com/ece350/assembler/profile/UserProfileService.java
+++ src/main/java/com/ece350/assembler/profile/UserProfileService.java
@@ -3,8 +3,10 @@ package com.ece350.assembler.profile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -19,5 +21,13 @@ public class UserProfileService {
     @GetMapping
     List<UserProfile> getUserProfiles() {
         return userProfileDataAccessService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        // 1. Check if image is not empty
+        // 2. If file is an image
+        // 3. The user exists in our database
+        // 4. Grab some metadata from file if any
+        // 5. Store the image in s3 and update database with s3 image link
     }
 }
