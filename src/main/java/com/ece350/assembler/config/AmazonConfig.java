96e45041d693d66768250cedc70dc9c5ea8cf9e4
max <max.smith@duke.edu>
2020-07-30 20:49:26 -0400

    Finished spoof endpoint

--- src/main/java/com/ece350/assembler/config/AmazonConfig.java
+++ src/main/java/com/ece350/assembler/config/AmazonConfig.java
@@ -3,6 +3,7 @@ package com.ece350.assembler.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource(value = "secrets.properties")
 public class AmazonConfig {
 
     @Value("${AWS_ACCESS_KEY}")
@@ -29,6 +29,7 @@ public class AmazonConfig {
 
         return AmazonS3ClientBuilder
                 .standard()
+                .withRegion(Regions.US_EAST_1)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
--- src/main/java/com/ece350/assembler/profile/UserProfileService.java
+++ src/main/java/com/ece350/assembler/profile/UserProfileService.java
@@ -1,10 +1,12 @@
 package com.ece350.assembler.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+@Service
 public class UserProfileService {
 
     private final UserProfileDataAccessService userProfileDataAccessService;
