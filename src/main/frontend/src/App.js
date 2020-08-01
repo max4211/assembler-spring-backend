f9ad9bd04495de025c1bce7f52dd5d00266933e5
max <max.smith@duke.edu>
2020-07-31 21:56:15 -0400

    Render data from db using hooks

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 import axios from "axios";
 
 const UserProfiles = () => {
+  const [userProfiles, setUserProfiles] = useState([]);
+  
   const fetchUserProfiles = () => {
     axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
       console.log(res);
+      setUserProfiles(res.data);
     })
   }
 
@@ -14,7 +17,15 @@ const UserProfiles = () => {
     fetchUserProfiles();
   }, []);
 
-  return <h1>Hello</h1>
+  return userProfiles.map((userProfile, index) => {
+
+    return (
+      <div key={index}>
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+      </div>
+    )
+  })
 }
 
 function App() {
