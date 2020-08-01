965f478abb6cad9d0618abc8f79a62e23d20f528
max <max.smith@duke.edu>
2020-08-01 18:33:30 -0400

    Completed file upload to S3 bucket

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -25,20 +25,41 @@ const UserProfiles = () => {
       <div key={index}>
         <h1>{userProfile.username}</h1>
         <p>{userProfile.userProfileId}</p>
-        <MyDropzone />
+        <MyDropzone {...userProfile} />
         <br></br>
       </div>
     )
   })
 }
 
-function MyDropzone() {
+function MyDropzone({ userProfileId }) {
   const onDrop = useCallback(acceptedFiles => {
     const file = acceptedFiles[0];
     console.log(file);
-    // Do something with the files
+    const formData = new FormData();
+    formData.append("file", file);
+    const myURL = concatURL();
+    axios.post(
+      myURL,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      }
+    ).then(() => {
+      console.log("file uploaded successfully")
+    }).catch(err => {
+      console.log(err);
+    });
   }, [])
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  function concatURL() {
+    const prefix = "http://localhost:8080/api/v1/user-profile/"
+    const suffix = "/image/upload";
+    return (prefix.concat(userProfileId, suffix));
+  }
 
   return (
     <div {...getRootProps()}>
--- src/main/java/com/ece350/assembler/profile/UserProfileService.java
+++ src/main/java/com/ece350/assembler/profile/UserProfileService.java
@@ -1,21 +1,26 @@
 package com.ece350.assembler.profile;
 
+import com.ece350.assembler.bucket.BucketName;
+import com.ece350.assembler.filestore.FileStore;
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
 
 @Service
 public class UserProfileService {
 
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
     @Autowired
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore) {
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
     @GetMapping
@@ -25,9 +30,51 @@ public class UserProfileService {
 
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
         // 1. Check if image is not empty
+        isEmpty(file);
+
         // 2. If file is an image
+        isImage(file);
+
         // 3. The user exists in our database
+        UserProfile user = getUserProfileOrThrow(userProfileId);
+
         // 4. Grab some metadata from file if any
+        Map<String, String> metadata = extractMetadata(file);
+
         // 5. Store the image in s3 and update database with s3 image link
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
+        String fileName = String.format("%s-%s", file.getName(), UUID.randomUUID());
+        try {
+            fileStore.save(path, fileName, Optional.of(metadata), file.getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @org.jetbrains.annotations.NotNull
+    private Map<String, String> extractMetadata(MultipartFile file) {
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+        return metadata;
+    }
+
+    private void isEmpty(MultipartFile file) {
+        if (file.isEmpty())
+            throw new IllegalStateException("Cannot upload empty file [ " + file.getSize() + "]");
+    }
+
+    private void isImage(MultipartFile file) {
+        if (!Arrays.asList(ContentType.IMAGE_JPEG.getMimeType(), ContentType.IMAGE_PNG.getMimeType(), ContentType.IMAGE_GIF.getMimeType()).contains(file.getContentType()))
+            throw new IllegalStateException(String.format("File must be an image, but of type: %s", file.getContentType()));
+    }
+
+    private UserProfile getUserProfileOrThrow(UUID userProfileId) {
+        return userProfileDataAccessService
+                .getUserProfiles()
+                .stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(String.format("User profile %s not found", userProfileId)));
     }
 }
