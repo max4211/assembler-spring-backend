bab017636fe02d37903fe3ba75c388e640c4da93
Max Smith <ms724@duke.edu>
2021-03-17 19:06:34 -0400

    Update mem file nops suffix

--- src/main/java/com/ece350/assembler/utility/io/MemFile.java
+++ src/main/java/com/ece350/assembler/utility/io/MemFile.java
@@ -1,9 +1,13 @@
 package com.ece350.assembler.utility.io;
 
+import com.ece350.assembler.utility.converter.SignExtend;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MemFile extends OutputFile {
+
+    private static final int TOTAL_LINES = 4096;
 
     public MemFile(String path, String outputBase, String digits, Output data) {
         super(path, data);
@@ -11,6 +15,7 @@ public class MemFile extends OutputFile {
         this.myHeader = new ArrayList<>();
         convertOutputToBase(outputBase, digits);
         insertHeader(this.myHeader, this.myOutput.getList());
+        endFile();
     }
 
     public MemFile(String outputBase, String digits, Output data) {
@@ -19,6 +24,7 @@ public class MemFile extends OutputFile {
         this.myHeader = new ArrayList<>();
         convertOutputToBase(outputBase, digits);
         insertHeader(this.myHeader, this.myOutput.getList());
+        endFile();
     }
 
     public MemFile(String outputBase, Output data) {
@@ -27,6 +33,15 @@ public class MemFile extends OutputFile {
         this.myHeader = new ArrayList<>();
         convertOutputToBase(outputBase);
         insertHeader(this.myHeader, this.myOutput.getList());
+        endFile();
+    }
+
+    private void endFile() {
+        int extendLines = TOTAL_LINES - this.myOutput.getList().size();
+        String bits = SignExtend.extend("0", 32);
+        for (int i = 0; i < Math.max(extendLines, 0); i ++) {
+            this.myOutput.add(bits);
+        }
     }
 
 }
